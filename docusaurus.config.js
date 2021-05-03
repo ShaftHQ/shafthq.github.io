0555183d0e071638b81eb8ce3381c52fd4f9e546
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2021-05-03 15:03:37 +0200

    fixing project directory

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'SHAFT User Guide',
   tagline: 'This is the official user guide for using SHAFT; The Unified Test Automation Engine.',
   url: 'https://mohabmohie.github.io',
-  baseUrl: '/apidocs/',
+  baseUrl: '/SHAFT_Engine_Docusaurus/',
   onBrokenLinks: 'throw',
   favicon: 'img/shaft.ico',
   organizationName: 'MohabMohie', // Usually your GitHub org/user name.
