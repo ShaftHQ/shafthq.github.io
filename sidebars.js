5ed1a87d880462033eb66b37e1472751ffeb4aea
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2022-07-04 09:21:51 +0200

    Revert "Features sondos (#12)"
    This reverts commit 9a381f1bde35e055f110a8d1a15d60656df6ebcb.

--- sidebars.js
+++ sidebars.js
@@ -1,22 +1,7 @@
 module.exports = {
-    someSidebar: {
-        Setup: ['Features', 'Prerequisites', 'IDE_preparation', 'Quick_Start'],
-        'Web GUI': ['Browser_Actions', 'Element_Actions'],
-        'Mobile GUI': ['Touch_Actions']
-        Validations: [
-            'Overview',
-            'Browser',
-            'Elements',
-            'Nums',
-            'Objects',
-            'Files',
-            'Response',
-            'ForceFail',
-        ],
-        'SHAFT Features': [
-            'features/retryFailedTests',
-            'features/BrowserRelatedFeatures',
-            'features/GUI_Elements_Features'
-        ],
-    },
+  someSidebar: {
+    "Setup": ['Features','Prerequisites','IDE_preparation'],
+    "Web GUI": ['Browser_Actions','Element_Actions', 'Touch_Actions'],
+    "Validations": ['Overview','Browser', 'Elements', 'Nums', 'Objects', 'Files', 'Response', 'ForceFail'],
+  },
 };
