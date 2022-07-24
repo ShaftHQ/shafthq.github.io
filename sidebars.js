ca955938c588883540dd7a8cebe21fd41cec3c20
SondosSaber <98236412+SondosSaber@users.noreply.github.com>
2022-07-24 15:58:22 +0200

    Updated Features (#17)
    * add to features and preparation
    
    * All validations are done
    
    * addingValidations
    
    * addingRightMenu
    
    * enhancements
    
    * enhancements
    
    * TouchActions
    
    * actions
    
    * initialize features
    
    * ElementsFeaturesPart1isDone
    
    * Updated features version
    
    Co-authored-by: aly <mohammed.aly@vodafone.com>
    Co-authored-by: mohammed saeed <mo.elsaeed.aly@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,22 @@
 module.exports = {
-  someSidebar: {
-    "Setup": ['Features','Prerequisites','IDE_preparation'],
-    "Web GUI": ['Browser_Actions','Element_Actions', 'Touch_Actions'],
-    "Validations": ['Overview','Browser', 'Elements', 'Nums', 'Objects', 'Files', 'Response', 'ForceFail'],
-  },
+    someSidebar: {
+        Setup: ['Features', 'Prerequisites', 'IDE_preparation', 'Quick_Start'],
+        'Web GUI': ['Browser_Actions', 'Element_Actions'],
+        'Mobile GUI': ['Touch_Actions'],
+        Validations: [
+            'Overview',
+            'Browser',
+            'Elements',
+            'Nums',
+            'Objects',
+            'Files',
+            'Response',
+            'ForceFail',
+        ],
+        'SHAFT Features': [
+            'features/retryFailedTests',
+            'features/BrowserRelatedFeatures',
+            'features/GUI_Elements_Features'
+        ],
+    },
 };
