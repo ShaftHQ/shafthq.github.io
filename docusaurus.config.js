7f5a96289ecac2bf3e917ea1ed423aa3dc5ca354
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2023-01-22 20:05:20 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,14 +40,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ShaftHQ/SHAFT_Engine_Docusaurus',
+            'https://github.com/ShaftHQ/SHAFT_Engine_Docusaurus/blob/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ShaftHQ/SHAFT_Engine_Docusaurus',
+            'https://github.com/ShaftHQ/SHAFT_Engine_Docusaurus/blob/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
