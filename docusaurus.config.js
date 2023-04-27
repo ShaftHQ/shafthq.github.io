dfa5094fed97058fe9a18c0ce20ddec1a51296ac
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2023-04-27 14:34:48 +0200

    Update docusaurus.config.js
    Signed-off-by: Mohab Mohie <Mohab.MohieElDeen@outlook.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'SHAFT User Guide',
   tagline: 'Stop reinventing the wheel. Start using SHAFT.',
   url: 'https://shafthq.github.io',
-  baseUrl: '/SHAFT_Engine_Docusaurus/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/shaft.ico',
